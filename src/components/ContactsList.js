b4375f8130777f33795baa50589d04d9eba8542e
OFFICE\avi.y <avi.y@tabooa.com>
2019-09-18 22:30:32 +0300

    cards css

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -4,7 +4,7 @@ import ContactDetails from './ContactDetails';
 const ContactsList = ({contacts}) => {
 
   return (
-    <div>
+    <div className="ui four stackable cards">
       {contacts.map((contact) => {
         return <ContactDetails key={contact.email} contact={contact}/>
       })}
